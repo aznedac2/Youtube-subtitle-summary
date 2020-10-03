c66415707b6ccd5b03abdf3ba1212fd7c5fde661
lee <aznedac2@gmail.com>
2020-10-03 22:27:17 +0900

    second impact

--- server.py
+++ server.py
@@ -68,4 +68,4 @@ def index():
         return render_template('index.html', src_url=src_url, text=text, fulltext = fulltext, trtext = trtext)
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run('0.0.0.0', port=3000, threaded=True)
